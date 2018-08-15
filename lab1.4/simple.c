24c78e7244f8964c2fd921cad39263ae4c245170
oscreader <oscreader@OSC>
2018-08-15 02:59:12 -0600

    modified

--- lab1.4/simple.c
+++ lab1.4/simple.c
@@ -5,14 +5,14 @@
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {
-       printk(KERN_INFO "Loading Module\n");
-
+       printk(KERN_INFO "Loading Arooniis Module\n");
        return 0;
 }
+	
 
 /* This function is called when the module is removed. */
 void simple_exit(void) {
-	printk(KERN_INFO "Removing Module\n");
+	printk(KERN_INFO "Removing Arooniis Module\n");
 }
 
 /* Macros for registering module entry and exit points. */
