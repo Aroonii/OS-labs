74313dde2aa85a03d5d48e968968d97e407b03d0
Arun Uppugunduri <arun.uppugunduri@gmail.com>
2018-09-18 05:50:22 -0600

    multiplier

--- lab1.9/multiplier.c
+++ lab1.9/multiplier.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_BUFFERS
+
+long* readMatrix(char *filename);
+
+
+
+int main(int argc, char **argv) {
+
+
+float *matrix;
+long i, size;
+
+size = atoi(argv[1]);
+matrix = malloc(sizeof(float)*size);
+
+for(i=0; i<size; i++) {
+	matrix[i] = i*2;
+}
+
+for (i=0; i<size; i++) {
+	printf("%f\n", matrix[i]);
+}
+	free(matrix);
+	
+
+
+
+
+
+return 0;
+
+}
+
+
+/*matrix*/
+long* readMatrix(char *filename) {
+
+
+
+
+}
+
+
+
 
